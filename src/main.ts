e8fbadbc475b347a8a662784100c25245e96a12e
nguyenquynh12345 <quynhnv@solidtech.vn>
2025-01-25 10:51:47 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,14 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:3002', // Chỉ cho phép từ frontend localhost:3002
+    origin: 'http://localhost:3002',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  // Serve static files from 'uploads' folder
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads', // Ảnh sẽ truy cập qua /uploads/fileName
+    prefix: '/uploads',
   });
 
   await app.listen(3333);
